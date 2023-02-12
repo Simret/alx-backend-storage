0ab423f6dc2c26d712dd693fbb847be0b19fb1ec
simret <simruineb@gmail.com>
2023-02-12 04:42:50 +0300

    Correction

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- Calculated Longetivity of bands with Glam rock
 
-SELECT band_name, COALESCE(split, 2020) - formed as lifespan
-FROM metal_bands
-WHERE style like '%Glam rock%';
+
+SELECT band_name, COALESCE(split, 2020) - formed as lifespan FROM
+metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
