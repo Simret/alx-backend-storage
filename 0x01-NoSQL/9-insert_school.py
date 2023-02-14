758b165fd6344a98863db664e4619bb271f6608a
simret <simruineb@gmail.com>
2023-02-15 00:14:50 +0300

    Insert documents

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Insert a document in Python"""
+
+import pymongo
+
+
+def insert_school(mongo_collection), **kwargs:
+    """Insert a document"""
+    documents = mongo_collection.Insert_one(kwargs)
+    return documents.inserted_id
